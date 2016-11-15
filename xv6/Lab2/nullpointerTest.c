9abafa367d4b5a3a0502aab18e366fc0041f96ef
Francisco <fmuno003@ucr.edu>
2016-11-14 22:16:14 -0800

    Testing NULL pointers

---
+++ xv6/Lab2/nullpointerTest.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+#include "param.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "traps.h"
+#include "memlayout.h"
+
+int main(int argc, char ** argv)
+{
+	
+	return 0;
+}
